f51d6691979ceeb7ff503ae251e4d840384a848c
deenanath <deena.nath@musictribe.com>
2025-06-14 13:36:40 +0530

    2 Sliding window program added

--- src/main/kotlin/collection/ArraySample.kt
+++ src/main/kotlin/collection/ArraySample.kt
@@ -1,15 +1,7 @@
 package collection
 
-import java.util.Arrays
-
 fun main() {
     val array: Array<Int> = arrayOf(1, 2, 3, 4)
     println(array.joinToString())
-    array[10] = 10
     println(array.size)
-    array[0] = 100
-    var a = Array(10) { 0 }
-    a.set(0,100)
-    a[1] = 100
-    val l = listOf(1, 0, 2)
 }
---
+++ src/main/kotlin/slidingwindow/FindTheSubarrayWithTheLargestSum.kt
@@ -0,0 +1,12 @@
+package slidingwindow
+
+fun main() {
+    val nums = intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)
+    val result = findLargestSum(nums)
+    println(result)
+}
+
+fun findLargestSum(arr: IntArray): Int {
+
+    return -1
+}
